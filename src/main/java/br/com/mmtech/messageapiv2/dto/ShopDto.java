c2e509162149e55ac5eaa160883fe1194f88f0ee
Matheus Mendes <matheushenriquesad@gmail.com>
2024-01-25 15:45:07 -0300

    MMT - Regra de negócio

--- src/main/java/br/com/mmtech/messageapiv2/dto/ShopDto.java
+++ src/main/java/br/com/mmtech/messageapiv2/dto/ShopDto.java
@@ -1,15 +1,13 @@
 package br.com.mmtech.messageapiv2.dto;
 
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
+import lombok.*;
+
 @Builder
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class ShopDto {
   private Long id;
   private String name;
